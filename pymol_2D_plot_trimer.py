47b9fd3d4d65367259070ae722d2c9aaa67223f8
weijiesun <weijie2@ualberta.ca>
2016-09-25 23:16:00 -0600

    Update the pymol plot

--- pymol_2D_plot_trimer.py
+++ pymol_2D_plot_trimer.py
@@ -2,11 +2,12 @@ import pymol
 from pymol import cmd 
 from HIV_point import Point
 import os
+from calculate_trimer_coordinates import distance
 
 pymol.finish_launching()
 
 def main():
-    print 'The default filename is points.'
+    print 'The default filename is points_2D.'
     fileName = raw_input('Input file name:')
     inputFile = open(fileName,'r')
     inputFileList = inputFile.read().splitlines()
@@ -14,9 +15,10 @@ def main():
     for i in inputFileList:
         tempList = i.split()
         pointMap[int(tempList[3])] = Point(float(tempList[0]),float(tempList[1]),float(tempList[2]),int(tempList[3]))
-    
     Radius = float(inputFileList[0].split()[2])
+    # print (pointMap.keys())
     r = distance(pointMap[1],pointMap[2])
+
     print 'Eg: R.pdb or R.pse'
     outPutFile = raw_input('Input output file name:')
 
@@ -29,12 +31,17 @@ def main():
 
     #Plot the first 4 trimer-center's coordinates.
     cmd.load('t1.pdb')
+    cmd.create('t0', 't1')
     #first trimerIndex
     cmd.select('/t1/PSDO')
     # cmd.color('blue','t1')
     cmd.show('line', 't1')
     cmd.rotate ('z', -30, 't1', camera=1)
     cmd.translate([0,0,Radius],'t1',camera=1)
+    my_dict = {'L':[]}
+    cmd.iterate_state(1, '/'+tempTName+'/PSDO', 'L.append( (x,y,z) )', space=my_dict)
+    cmd.translate([pointMap[i].x-my_dict['L'][0][0],pointMap[i].y-my_dict['L'][0][1],pointMap[i].z-my_dict['L'][0][2]],tempTName,camera=1)
+    
 
     #second trimerIndex
     cmd.create('t2', 't1')
@@ -46,35 +53,35 @@ def main():
     cmd.orient('t2')
     cmd.rotate('z', 60, 't2', camera=1)
     cmd.rotate('z', -30 )
-    #this angle changes to bring involved pseudo atoms to horizontal positions
-    cmd.rotate('y', 4.726, 't2', camera=1)
-    cmd.rotate('x', 0.004, 't2', camera=1)
+    # this angle changes to bring involved pseudo atoms to horizontal positions
+    # cmd.rotate('y', 4.726, 't2', camera=1)
+    # cmd.rotate('x', 0.004, 't2', camera=1)
     # because we are rotating only one trimer, the 2 central pseudo atoms will not align. correction commands are:
-    cmd.translate([0, -0.0440605685747639, -2.14176156973178], 't2', camera=1)
+    # cmd.translate([0, -0.0440605685747639, -2.14176156973178], 't2', camera=1)
     # cmd.alter('/t2//A', chain='D')
     # cmd.alter('/t2//B', chain='E')
     # cmd.alter('/t2//C', chain='F')
 
-    #third trimerIndex
+    # third trimerIndex
     cmd.create('t3','t2')
     cmd.select('/t3/PSDO')
     cmd.show('line','t3')
     cmd.origin('t1')
     cmd.rotate(pointMap[1].getCoordinate(),120,'t3',camera=1)
 
-    #forth trimerIndex
+    # forth trimerIndex
     cmd.create('t4','t2')
     cmd.select('/t4/PSDO')
     cmd.show('line','t4')
     cmd.origin('t1')
     cmd.rotate(pointMap[1].getCoordinate(),240,'t4',camera=1)
 
-    #According to the first 4 trimer-center coordinates, to plot the following trimers.
+    # According to the first 4 trimer-center coordinates, to plot the following trimers.
     keyList = pointMap.keys()
     keyList.sort()
-    # testCount = 1
+    testCount = 1
     for i in keyList:
-        if (i>4):
+        if (i>4 and i <20):
             # testCount += 1
             tempTName = 't'+str(i)
             if (i%2)==1:
@@ -92,6 +99,9 @@ def main():
             cmd.show('line',tempTName)
             cmd.origin(tempTFatherName)
             cmd.rotate(pointMap[(i-1)//2].getCoordinate(),tempDegree,tempTName,camera=1)
-
+            my_dict = {'L':[]}
+            cmd.iterate_state(1, '/'+tempTName+'/PSDO', 'L.append( (x,y,z) )', space=my_dict)
+            cmd.translate([pointMap[i].x-my_dict['L'][0][0],pointMap[i].y-my_dict['L'][0][1],pointMap[i].z-my_dict['L'][0][2]],tempTName,camera=1)
+            
     cmd.save(outPutFile)
 main()
